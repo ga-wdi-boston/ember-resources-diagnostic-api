278be028bcdd40ba7a27006f863024822eb847da
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-03-25 10:12:12 -0400

    Nest Comment routes inside Blogpost routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :comments, except: [:new, :edit]
-  resources :blogposts, except: [:new, :edit]
+  resources :blogposts, except: [:new, :edit] do
+    resources :comments, except: [:new, :edit]
+  end
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
