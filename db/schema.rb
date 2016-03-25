8ae01da61cb97dcc9f05f8216ed2ebcf64a17224
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-03-25 10:33:32 -0400

    Drop DB and re-run migrations
    Old table was left behind by previously ran migration and not removed.
    Doing the above got rid of it.

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,6 @@ ActiveRecord::Schema.define(version: 20160325142104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "blog_posts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "blogposts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
