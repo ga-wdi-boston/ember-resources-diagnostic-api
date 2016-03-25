8c8d2c6f35becca4f773271233a9378f23b64e45
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-03-25 11:01:09 -0400

    Un-nest the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :blogposts, except: [:new, :edit] do
-    resources :comments, except: [:new, :edit]
-  end
+  resources :blogposts, except: [:new, :edit]
+  resources :comments, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
